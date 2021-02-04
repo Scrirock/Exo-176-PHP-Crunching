54c995a7e56389aa64eb39ced5c57d3b9222268d
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-02-04 10:21:38 +0100

    json illisible...

--- index.php
+++ index.php
@@ -29,5 +29,17 @@
 
     echo "nbr mot qui finnissent par q: ".$motQ;
     echo "<br><br>";
-    
+
+    $string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
+    $brut = json_decode($string, true);
+    $top = $brut["feed"]["entry"]; # liste de films
+/*
+    foreach ($top as $values){
+        foreach ($values as $key => $truk){
+            foreach ($truk as $machin => $autreMachin){
+                echo $machin.": ".$autreMachin."<br>";
+            }
+        }
+    }
+*/
 ?>
